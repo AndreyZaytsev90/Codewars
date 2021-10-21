d716bfd77064d5c0a21d672fe9eb311710f0d16e
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-21 20:28:49 +0300

    added new file

---
+++ The 'if' function.js
@@ -0,0 +1,18 @@
+/*function _if(bool, func1, func2) {
+  if (
+    function func1() {
+      console.log("True");
+    }
+  ) {
+    return true;
+  }
+  if (
+    function func2() {
+      console.log("False");
+    }
+  ) {
+    return false;
+  }
+}
+*/
+// до лучших времен
