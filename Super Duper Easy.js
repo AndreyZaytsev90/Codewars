352e2f230ea4c002018d2450eeaebd65042cfa93
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-21 21:46:57 +0300

    added new file

---
+++ Super Duper Easy.js
@@ -0,0 +1,13 @@
+/*function problem(x) {
+  if (x >= 0) {
+    return x * 50 + 6;
+  }
+
+  if (x < 0) {
+    return x * 50 + 6;
+  }
+  if ((x = " ")) {
+    return Error("Error");
+  }
+}*/
+// до лучших времен
