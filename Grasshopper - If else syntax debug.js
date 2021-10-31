170055f23eb9179ca4b797d5d7215743ef1d7be5
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-10-31 21:43:03 +0300

    added new files

--- Grasshopper - If else syntax debug.js
+++ Grasshopper - If else syntax debug.js
@@ -1,8 +1,7 @@
 function checkAlive(health) {
-  if ((health > 0) | (health <= 10)) {
+  if (health > 0 && health <= 10) {
     return true;
   } else {
     return false;
   }
 }
-// не решено...(
--- Super Duper Easy.js
+++ Super Duper Easy.js
@@ -1,15 +1,7 @@
 function problem(x) {
-  /* if (x >= 0) {
+  if (typeof x === "string" || x instanceof String) {
+    return "Error";
+  } else {
     return x * 50 + 6;
-  }
-
-  if (x < 0) {
-    return x * 50 + 6;
-  }
-  if ((x = " ")) {
-    return (typeof x === string) ? "Error"
-  }*/
-  function problem(x) {
-    return typeof x === string ? "Error" : x * 50 + 6;
   }
 }
