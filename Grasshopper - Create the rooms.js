9f3473c5be275bbc690e7f0c946c22452baeb3d9
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2021-11-05 19:54:45 +0300

    added new file

---
+++ Grasshopper - Create the rooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+const rooms = {
+  first: {
+    description: "This is the first room",
+    name: "one",
+    completed: "",
+  },
+  second: {
+    description: "This is the second room",
+    name: "two",
+    completed: "",
+  },
+  third: {
+    description: "This is the third room",
+    name: "three",
+    completed: "",
+  },
+};
